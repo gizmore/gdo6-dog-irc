9e0d2868194324c01d62e3d3ad3eef4309254aae
gizmore <gizmore@wechall.net>
2022-11-16 06:57:34 +0100

    '7.0.1-r1708'

--- Method/JoinServer.php
+++ Method/JoinServer.php
@@ -31,7 +31,7 @@ final class JoinServer extends DOG_IRCCommand
     public function gdoParameters() : array
     {
         return array(
-            GDT_Url::make('url')->schemes('irc', 'ircs')->allowLocal()->notNull(),
+            GDT_Url::make('url')->schemes('irc', 'ircs')->allowInternal()->notNull(),
             GDT_Username::make('nickname')->initial($this->getDefaultNickname()),
             GDT_Secret::make('password'),
         );
