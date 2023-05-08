33d56ab618f8cc540b6e7590e0956a70013402bd
gizmore <gizmore@wechall.net>
2023-05-08 18:35:54 +0200

    '7.0.3-r1790 It is getting super unit tested and stable :)'

--- IRCLib.php
+++ IRCLib.php
@@ -1,21 +1,41 @@
 <?php
+declare(strict_types=1);
 namespace GDO\DogIRC;
 
+use GDO\Core\Website;
 use GDO\Dog\Dog;
 
 /**
  * IRC utility functions.
  *
  * @author gizmore
+ * @version 7.0.3
  */
 final class IRCLib
 {
+	final public const MAX_MSG_LEN = 420;
 
 	final public const CTCP = "\x01";
 
 	final public const BOLD = "\x02";
 
 	final public const ITALIC = "\x1F";
+
+	final public const WHITE = '00';
+
+	final public const BLACK = '01';
+
+	final public const BLUE = '02';
+
+	final public const GREEN = '03';
+
+	final public const RED = '04';
+
+	final public const BROWN = '05';
+
+	final public const MAGENTA = '06';
+
+	final public const ORANGE = '07';
 
 
 	public static function bold(string $s): string
@@ -26,19 +46,25 @@ final class IRCLib
 
 	public static function italic(string $s): string
 	{
-		$b = self::ITALIC;
-		return "{$b}{$s}{$b}";
+		$i = self::ITALIC;
+		return "{$i}{$s}{$i}";
+	}
+
+	/**
+	 * @TODO: Turn a string into an IRC colored string.
+	 */
+	public static function colored(string $s, string $ircColor): string
+	{
+		Website::errorRaw('IRCLIB', 'IRCLIB Color not working yet. Color: ' . $ircColor);
+		return $s;
 	}
 
 
-	public static function permissionFromUsername($userName)
+	public static function permissionFromUsername(string $userName): string
 	{
 		$map = self::mapCharPermission();
 		$char = $userName[0];
-		if (isset($map[$char]))
-		{
-			return $map[$char];
-		}
+		return $map[$char]?: '';
 	}
 
 	public static function mapCharPermission(): array
@@ -46,12 +72,13 @@ final class IRCLib
 		return [
 			'+' => Dog::VOICE,
 			'%' => Dog::HALFOP,
-			'&' => Dog::OPERATOR,
 			'@' => Dog::OPERATOR,
+			'&' => Dog::OPERATOR,
+			'!' => Dog::OPERATOR,
 		];
 	}
 
-	public static function trimmedUsername($userName)
+	public static function trimmedUsername(string $userName): string
 	{
 		$chars = implode('', array_keys(self::mapCharPermission()));
 		return ltrim($userName, $chars);
@@ -62,10 +89,10 @@ final class IRCLib
 	 *
 	 * @return string[]
 	 */
-	public static function splitMessage(string $text, int $len = 420): array
+	public static function splitMessage(string $text, int $len = self::MAX_MSG_LEN): array
 	{
+		$end1 = 0;
 		$chunks = [];
-		$end1 = $end2 = 0;
 		while (true)
 		{
 			if (mb_strlen($text) <= $len)
@@ -82,11 +109,10 @@ final class IRCLib
 				}
 				$chunks[] = mb_substr($text, 0, $end1);
 				$text = mb_substr($text, $end1);
-				$end1 = $end2 = 0;
+				$end2 = 0;
 			}
 			$end1 = $end2;
 		}
-		return $chunks;
 	}
 
 }
--- Module_DogIRC.php
+++ Module_DogIRC.php
@@ -1,8 +1,15 @@
 <?php
+declare(strict_types=1);
 namespace GDO\DogIRC;
 
 use GDO\Core\GDO_Module;
 
+/**
+ * IRC Connector and basics for the Dog GDOv7 Chatbot.
+ * @author gizmore
+ * @version 7.0.3
+ * @since 3.2.0
+ */
 final class Module_DogIRC extends GDO_Module
 {
 
