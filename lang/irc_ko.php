d857538ab4e181749ec904f0d79cbd3f5b2841ee
gizmore <gizmore@wechall.net>
2026-09-18 04:07:46 +0200

    'more korean'

---
+++ lang/irc_ko.php
@@ -0,0 +1,13 @@
+<?php
+return [
+	'dog_help_join_channel' => '#BOT#을 IRC 채널에 참여시킵니다.',
+	'err_dog_already_in_room' => '나는 이미 %s 안에 있습니다.',
+	'msg_join_irc_channel' => '%s에 가입하려고 합니다.',
+	'dog_help_part_channel' => '#BOT#이 IRC 채널에서 나가도록 합니다.',
+	'msg_part_irc_channel' => '저는 이제 %s을(를) 떠나고 스스로 돌아오지 않을 것입니다.',
+	'dog_help_part_join_network' => '#BOT#을 새 네트워크에 연결합니다.',
+	'msg_dog_joining_irc_network' => '%s 네트워크에 연결하는 중...',
+	'err_dog_join_network_failed' => '네트워크 연결에 실패했습니다.',
+	'msg_dog_irc_server_connected' => '%s에 대한 연결이 설정되었습니다. 인증 중...',
+	'msg_dog_irc_server_fresh' => '%s에 %s로 성공적으로 연결했습니다. 권한을 얻으려면 /msg %2$s super %3$s를 시도하세요.',
+];
